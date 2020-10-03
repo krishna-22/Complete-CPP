6493a57e666321dda228854018e7ba61deff57b4
krishna-22 <42892516+krishna-22@users.noreply.github.com>
2020-10-03 18:17:33 +0530

    Create reading-input.cpp

---
+++ reading-input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include<cstring>
+#include<cmath>
+
+int main()
+{
+    
+    string name;
+    cout<<"enter age: "<<endl;
+    int age;
+    cin>>age;
+    cout<<"enter your name: "<<endl;
+    getline(cin,name);
+    
+    cout<<name;
+    return 0;
+}
