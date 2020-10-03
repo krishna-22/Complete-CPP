cd1a1a6f8a3e79e72c02c2873a320565c1af16ae
krishna-22 <42892516+krishna-22@users.noreply.github.com>
2020-10-03 18:04:00 +0530

    Create math_functions.cpp

---
+++ math_functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include<cstring>
+#include<cmath>
+
+int main()
+{
+    cout<<pow(2,3)<<endl;
+    cout<<sqrt(36)<<endl;
+    cout<<round(4.56)<<endl;
+    cout<<ceil(4.1)<<endl;
+    cout<<floor(4.1)<<endl;
+    cout<<fmax(3,5)<<endl;
+    cout<<abs(-4)<<endl;
+    return 0;
+}
